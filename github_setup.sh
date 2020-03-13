b27e2543fedaaa2870f9802c97537ac47ccab64f
Julie Allinson <julie@notch8.com>
2020-03-13 21:19:19 +0000

    change my email

--- github_setup.sh
+++ github_setup.sh
@@ -7,7 +7,7 @@ sudo chmod 644 ~/.ssh/id_rsa.pub
 sudo chmod 700 ~/.ssh/id_rsa
 
 git config --global user.name "Julie Allinson"
-git config --global user.email "julie.allinson@london.ac.uk"
+git config --global user.email "julie@notch8.com"
 git config --global core.excludesfile ~/.gitignore
 git config --global core.editor vim
 
